f4d65dd3e2c49380f869be37aa084f2ca882ab62
locutor41-beep <locutor41@gmail.com>
2025-10-08 08:29:41 -0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+const CACHE = 'disk-mensagem-v1';
+const ASSETS = [
+  '/', '/index.html', '/admin.html',
+  '/manifest.webmanifest',
+  '/icons/icon-192.png', '/icons/icon-512.png',
+  '/app.js' // adicione também seus CSS/JS principais
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
